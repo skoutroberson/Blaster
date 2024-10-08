2be6dc172d0cb652514f52b8b873a2a410ee0d7c
Skout Roberson <skout.roberson@gmail.com>
2024-10-08 14:40:35 -0700

    Created weapon class
    The weapon class will be used as a base class for all weapons that
    players use.

--- Blaster/Source/Blaster/Weapon/Weapon.h
+++ Blaster/Source/Blaster/Weapon/Weapon.h
@@ -6,21 +6,37 @@
 #include "GameFramework/Actor.h"
 #include "Weapon.generated.h"
 
+UENUM(BlueprintType)
+enum class EWeaponState : uint8
+{
+	EWS_Initial		UMETA(DisplayName = "Initial State"),
+	EWS_Equipped	UMETA(DisplayName = "Equipped"),
+	EWS_Dropped		UMETA(DisplayName = "Dropped"),
+	EWS_MAX			UMETA(DisplayName = "DefaultMAX"),
+};
+
 UCLASS()
 class BLASTER_API AWeapon : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
 	AWeapon();
-
+	virtual void Tick(float DeltaTime) override;
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	USkeletalMeshComponent* WeaponMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	class USphereComponent* AreaSphere;
+
+	UPROPERTY(VisibleAnywhere)
+	EWeaponState WeaponState;
+
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	
 
 };
