923dbdadf639d73bf4d482f0588f631148f726e1
Skout Roberson <skout.roberson@gmail.com>
2024-10-19 21:07:58 -0700

    Crosshair is drawn on screen and weapon rotation fix
    Locally controlled players have their weapon rotate exactly where they
    are aiming.

--- Blaster/Source/Blaster/Character/BlasterAnimInstance.cpp
+++ Blaster/Source/Blaster/Character/BlasterAnimInstance.cpp
@@ -67,5 +67,12 @@ void UBlasterAnimInstance::NativeUpdateAnimation(float DeltaTime)
 		BlasterCharacter->GetMesh()->TransformToBoneSpace(FName("hand_r"), LeftHandTransform.GetLocation(), FRotator::ZeroRotator, OutPosition, OutRotation);
 		LeftHandTransform.SetLocation(OutPosition);
 		LeftHandTransform.SetRotation(FQuat(OutRotation));
+
+		if (BlasterCharacter->IsLocallyControlled())
+		{
+			bLocallyControlled = true;
+			FTransform RightHandTransform = EquippedWeapon->GetWeaponMesh()->GetSocketTransform(FName("hand_r"), ERelativeTransformSpace::RTS_World);
+			RightHandRotation = UKismetMathLibrary::FindLookAtRotation(RightHandTransform.GetLocation(), RightHandTransform.GetLocation() + (RightHandTransform.GetLocation() - BlasterCharacter->GetHitTarget()));
+		}
 	}
 }
