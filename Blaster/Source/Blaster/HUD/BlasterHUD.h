e2c75b79c36fe90c7f9930b1a0a74baad8768703
Skout Roberson <skout.roberson@gmail.com>
2024-10-29 00:34:54 -0700

    Overrode OnMatchStateSet() in BlasterGameMode
    HUD now shows when the MatchState gets set to InProgress.

--- Blaster/Source/Blaster/HUD/BlasterHUD.h
+++ Blaster/Source/Blaster/HUD/BlasterHUD.h
@@ -34,11 +34,13 @@ public:
 	UPROPERTY(EditAnywhere, Category = "Player Stats")
 	TSubclassOf<class UUserWidget> CharacterOverlayClass;
 
+	UPROPERTY()
 	class UCharacterOverlay* CharacterOverlay;
+
+	void AddCharacterOverlay();
 
 protected:
 	virtual void BeginPlay() override;
-	void AddCharacterOverlay();
 
 private:
 	FHUDPackage HUDPackage;
